9c2dc06f7da9aa484223ab6d27ac5ae16fff9330
Mauricio Lopez <mlopez@escorial.com.ar>
2025-03-11 15:52:49 -0300

    desarrollo con datos de prueba, manejo de pallets y productos, desplegable tipo producto

--- app/src/main/java/com/escorial/pallet_cocinas/MainActivity.kt
+++ app/src/main/java/com/escorial/pallet_cocinas/MainActivity.kt
@@ -5,20 +5,29 @@ import android.util.Log
 import android.view.KeyEvent
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import android.widget.AdapterView
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.ListView
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var kitchenEditText: EditText
+    lateinit var selectedProductType: String
+
+    lateinit var productEditText: EditText
     lateinit var palletEditText: EditText
     lateinit var palletTextView: TextView
-    lateinit var kitchensListView: ListView
-    lateinit var kitchensAdapter: ArrayAdapter<String>
-    var kitchensList: ArrayList<String> = ArrayList()
+    lateinit var productsListView: ListView
+    lateinit var productSpinner: Spinner
+    lateinit var submitButton: Button
+
+    lateinit var productsAdapter: ArrayAdapter<Product>
+    var productsList: ArrayList<Product> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,21 +35,22 @@ class MainActivity : AppCompatActivity() {
 
         loadControls()
 
-        kitchenEditText.setOnEditorActionListener(createEnterListener("kitchen"))
+        productEditText.setOnEditorActionListener(createEnterListener("product"))
         palletEditText.setOnEditorActionListener(createEnterListener("pallet"))
+        submitButton.setOnClickListener { submit() }
 
         palletEditText.requestFocus()
     }
 
     private fun createEnterListener(type: String): TextView.OnEditorActionListener {
         return TextView.OnEditorActionListener { v, actionId, event ->
-            Log.d("EnterListener", "Enter key event - type: $type, actionId: $actionId, event: $event")
+            log("EnterListener", "Enter key event - type: $type, actionId: $actionId, event: $event")
             if (actionId == EditorInfo.IME_ACTION_DONE || actionId == 5 ||
                 (event?.keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_DOWN)
             ) {
-                Log.d("EnterListener", "Enter key event detected")
+                log("EnterListener", "Enter key event detected")
                 when (type) {
-                    "kitchen" -> handleKitchen()
+                    "product" -> handleProduct()
                     "pallet" -> handlePallet()
                 }
 
@@ -54,66 +64,145 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun handleKitchen() {
-        Log.d("Kitchen", "Kitchen: ${kitchenEditText.text}")
-        val kitchenCode = kitchenEditText.text.toString()
-        var kitchens = Kitchen.getSampleKitchens()
-        if (kitchenCode.isEmpty()) {
-            Log.d("Kitchen", "Kitchen is empty")
-            kitchenEditText.text.clear()
-            kitchenEditText.requestFocus()
+    private fun handleProduct() {
+        log("Product", "Product: ${productEditText.text}")
+        val productSerial = productEditText.text.toString()
+        var products = ArrayList<Product>()
+        if (selectedProductType == "COCINA") {
+            log("Product", "Product type: COCINA")
+            products = Product.getKitchens()
+        }
+        else if (selectedProductType == "TERMO/CALEFON") {
+            log("Product", "Product type: TERMO/CALEFON")
+            products = Product.getHeaters()
+        }
+        else {
+            Toast.makeText(this@MainActivity, "Debe seleccionar un tipo de producto para continuar", Toast.LENGTH_LONG).show()
             return
         }
-        if (kitchens.isEmpty()) {
-            Log.d("Kitchen", "Kitchen list is empty")
-            kitchenEditText.text.clear()
-            kitchenEditText.requestFocus()
+        if (productSerial.isEmpty()) {
+            log("Product", "Product is empty")
+            productEditText.text.clear()
+            productEditText.requestFocus()
             return
         }
-        if (kitchensList.count() == 8) {
-            Log.d("Kitchen", "Kitchen list is full")
-            kitchenEditText.text.clear()
-            kitchenEditText.requestFocus()
+        if (products.isEmpty()) {
+            log("Product", "Product list is empty")
+            productEditText.text.clear()
+            productEditText.requestFocus()
             return
         }
-        if (kitchens.find { it.code == kitchenCode } == null) {
-            Log.d("Kitchen", "Kitchen not found")
-            kitchenEditText.text.clear()
-            kitchenEditText.requestFocus()
+        if (productsList.count() == 8) {
+            log("Product", "Product list is full")
+            productEditText.text.clear()
+            productEditText.requestFocus()
             return
         }
-        Log.d("Kitchen", "Kitchen found")
-        kitchensList.add(kitchenCode)
-        kitchenEditText.text.clear()
-        kitchensAdapter.notifyDataSetChanged()
-        kitchenEditText.requestFocus()
+        val product = products.find { it.serial == productSerial }
+        if (product == null) {
+            log("Product", "Product not found")
+            productEditText.text.clear()
+            productEditText.requestFocus()
+            return
+        }
+        if (product.palletized) {
+            log("Product", "Product palletized")
+            productEditText.text.clear()
+            productEditText.requestFocus()
+            return
+        }
+        if (productsList.contains(product)){
+            log("Product", "Product selected before")
+            productEditText.text.clear()
+            productEditText.requestFocus()
+            return
+        }
+        log("Product", "Product found")
+        if (!productsList.isEmpty() && productsList.last().code != product.code) {
+            log("Product", "Product type incorrect")
+            productEditText.text.clear()
+            productEditText.requestFocus()
+            return
+        }
+        productsList.add(product)
+        productEditText.text.clear()
+        productsAdapter.notifyDataSetChanged()
+        productEditText.requestFocus()
     }
 
     private fun handlePallet() {
-        Log.d("Pallet", "Pallet: ${palletEditText.text}")
-        val pallets = Pallet.getSamplePallets()
-        if (pallets.find { it.code == palletEditText.text.toString() } == null) {
-            Log.d("Pallet", "Pallet not found")
+        log("Pallet", "Pallet: ${palletEditText.text}")
+        val pallets = Pallet.getPallets()
+        val pallet = pallets.find { it.code == palletEditText.text.toString() }
+        if (pallet == null) {
+            log("Pallet", "Pallet not found")
             palletEditText.text.clear()
             palletEditText.requestFocus()
             return
         }
-        Log.d("Pallet", "Pallet found")
-        palletTextView.text = "Pallet: ${palletEditText.text}"
+        log("Pallet", "Pallet found")
+        if (pallet.products != null && pallet.products!!.isNotEmpty()) {
+            for (product in pallet.products) {
+                productsList.add(product)
+            }
+            productsAdapter.notifyDataSetChanged()
+        }
+
+        palletTextView.text = "Pallet: ${palletEditText.text} (${pallet.products?.firstOrNull()?.code ?: "vacio"})"
         palletEditText.isEnabled = false
-        kitchenEditText.isEnabled = true
-        kitchenEditText.requestFocus()
+        productEditText.isEnabled = true
+        productEditText.requestFocus()
+    }
+
+    private fun submit() {
+        if (productsList.isEmpty()) return
+        for (product in productsList) {
+            if (selectedProductType == "COCINA") Product.updateStockedKitchen(product.serial, true)
+            else if (selectedProductType == "TERMO/CALEFON") Product.updateStockedHeater(product.serial, true)
+        }
+        Pallet.updateProductsInPallet(palletEditText.text.toString(), productsList)
+        val intent = intent
+        finish()
+        startActivity(intent)
     }
 
     private fun loadControls() {
-        Log.d("LoadControls", "Loading controls")
+        log("LoadControls", "Loading controls")
         palletEditText = findViewById(R.id.palletEditText)
-        kitchenEditText = findViewById(R.id.kitchenEditText)
-        kitchenEditText.isEnabled = false
+        productEditText = findViewById(R.id.productEditText)
+        productEditText.isEnabled = false
         palletTextView = findViewById(R.id.palletTextView)
-        kitchensAdapter =
-            ArrayAdapter(this, android.R.layout.simple_list_item_1, kitchensList)
-        kitchensListView = findViewById(R.id.kitchensListView)
-        kitchensListView.adapter = kitchensAdapter
+        submitButton = findViewById(R.id.submitButton)
+
+        productSpinner = findViewById(R.id.productSpinner)
+        configProductSpinner()
+
+        productsListView = findViewById(R.id.productsListView)
+        productsAdapter =
+            ArrayAdapter(this, android.R.layout.simple_list_item_1, productsList)
+        productsListView.adapter = productsAdapter
+    }
+
+    private fun configProductSpinner() {
+        val options = listOf("COCINA", "TERMO/CALEFON")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, options)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        productSpinner.adapter = adapter
+        productSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: android.view.View?, position: Int, id: Long) {
+                val selectedOption = parent.getItemAtPosition(position).toString()
+                Toast.makeText(this@MainActivity, "Seleccionaste: $selectedOption", Toast.LENGTH_SHORT).show()
+                selectedProductType = selectedOption
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                Toast.makeText(this@MainActivity, "No seleccionaste nada", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun log(title: String, message: String) {
+        Log.d(title, message)
+        Toast.makeText(this, "$title: $message", Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/escorial/pallet_cocinas/Pallet.kt
+++ app/src/main/java/com/escorial/pallet_cocinas/Pallet.kt
@@ -3,13 +3,21 @@ package com.escorial.pallet_cocinas
 data class Pallet(
     val id: Int,
     val code: String,
-    val kitchenId: Int
+    var products: ArrayList<Product>?
 ) {
     companion object {
-        fun getSamplePallets(): List<Pallet> {
-            return listOf(
-                Pallet(1, "8126116731721", 1)
-            )
+        private val pallets = arrayListOf<Pallet>(
+            Pallet(1, "1", null),
+            Pallet(2, "2", null),
+            Pallet(3, "3", null),
+            Pallet(4, "4", null),
+            Pallet(5, "5", null)
+        )
+
+        fun updateProductsInPallet(palletCode: String, productsToAdd: ArrayList<Product>) {
+            pallets.find { it.code == palletCode }?.products = productsToAdd
         }
+
+        fun getPallets(): ArrayList<Pallet> = pallets
     }
 }
