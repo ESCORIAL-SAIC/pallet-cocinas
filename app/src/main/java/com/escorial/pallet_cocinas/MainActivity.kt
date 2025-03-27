ce77423798d2c8774c8f3ccc56af23b801012164
Ignacio Ferrari <iferrari@ESCORIALSA.COM.AR>
2025-03-27 15:20:33 -0300

    Modif de input de texto y rediseñe devuelta el spinner

--- app/src/main/java/com/escorial/pallet_cocinas/MainActivity.kt
+++ app/src/main/java/com/escorial/pallet_cocinas/MainActivity.kt
@@ -366,14 +366,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun configProductSpinner() {
-        val options = listOf("COCINA", "TERMO/CALEFON")
-        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, options)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        //val options = listOf("COCINA", "TERMO/CALEFON")
+        val options2 = resources.getStringArray(R.array.tipo_producto)
+        //val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, options)
+        val adapter = ArrayAdapter(this, R.layout.product_dropdown_item, options2)
+        //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        adapter.setDropDownViewResource(R.layout.product_dropdown_item)
 
         //Posibles modificaciones para reemplazar a Spinner
-        val options2 = resources.getStringArray(R.array.tipo_producto)
-        val adapter2 = ArrayAdapter(this, R.layout.product_dropdown_item, options2)
-        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        //val adapter2 = ArrayAdapter(this, R.layout.product_dropdown_item, options2)
+        //adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
 
         productSpinner.adapter = adapter
         productSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
